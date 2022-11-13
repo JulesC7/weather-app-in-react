edc062206c390df8b4ca5f7a1c5683a197df8b2d
JulesC7 <109908177+JulesC7@users.noreply.github.com>
2022-11-13 13:54:26 +0100

    updated icons

--- src/Weather.js
+++ src/Weather.js
@@ -24,6 +24,7 @@ export default function Weather(props) {
       temperature: Math.round(response.data.main.temp),
       humidity: response.data.main.humidity,
       feelsLike: Math.round(response.data.main.feels_like),
+      icon: response.data.weather[0].icon,
       imgUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
